3e4f195eba9a84691566264e447b3163b4952ac7
Motoaki Tanaka <katachiwotodomete@gmail.com>
2023-04-03 05:47:07 +0000

    mod

--- amplifyPull.sh
+++ amplifyPull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-AMPLIFY="{\"projectName\":\"insurtech-knockun-app\",\"appId\":\"ddk19zql38o5c\",\"envName\":\"staging\",\"defaultEditor\":\"code\"}"
+AMPLIFY="{\"projectName\":\"cicdsample\",\"appId\":\"d1mcteum1ndmhc\",\"envName\":\"staging\",\"defaultEditor\":\"code\"}"
 FRONTEND="{\"frontend\":\"javascript\",\"framework\":\"react\",\"config\":{\"SourceDir\":\"src/components\",\"DistributionDir\":\"build\",\"BuildCommand\":\"npm run-script build\",\"StartCommand\":\"npm run-script start\"}}"
 AWSCLOUDFORMATIONCONFIG="{\"configLevel\":\"project\",\"useProfile\":true,\"profileName\":\"default\",\"region\":\"ap-northeast-1\"}"
 PROVIDERS="{\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG}"
